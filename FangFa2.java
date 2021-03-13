e5c409b89ad6aa0198c80306624189a7ee13c53e
song_biao_123 <759403720@qq.com>
2021-03-13 20:43:37 +0800

    交换的错误代码

---
+++ FangFa2.java
@@ -0,0 +1,25 @@
+public class FangFa2 {
+    //错误代码，并不能完成交换
+/*
+    public static void swap(int x,int y){
+        int temp;
+        temp=x;
+        x=y;
+        y=temp;
+    }
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        swap(a,b);
+        System.out.println("a="+a+"b="+b);
+    }
+*/
+
+
+    /**
+     * 下面给出交换的正确代码
+     * java中的数组为引用数据类型，相当于c语言中的指针，引用实际上就是一个变量，它用来存储地址
+     *
+     */
+}
+
